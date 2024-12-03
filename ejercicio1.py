f7f2e7dc14b0937e2501a993dcda1eae9ec35e1c
immaaaaagot <immgotros@alu.edu.gva.es>
2024-12-03 10:15:15 +0100

    acabado

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,4 @@
 base= int(input("Dime la base:"))
 altura=int(input("Dime la altura:"))
-area= (base*altura)/2
+area= round((base*altura)/2 ,2)
 print("El área del triángulo es:" + str(area))
---
+++ volumen_cilindro.py
@@ -0,0 +1,5 @@
+from math import*
+radio= int(input("Dime el radio:"))
+altura=int(input("Dime la altura:"))
+volumen= (pi*radio**2*altura)
+print("El volumen del cilindro:" + str(volumen))
